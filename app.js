926b95cebdc033e97eb2888754df7739ee9ce060
BuiTuanVu <vubui2010@gmail.com>
2019-12-24 16:52:32 +0700

    build contruct for project

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+const bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+
+//set engine
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.send('Hello world1')
+})
+app.listen(3000);
+console.log('Server is running at port: 3000');
