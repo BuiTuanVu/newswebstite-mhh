9ac7273bc2b38941ef56088e170600854ae404a8
Anh Từ <44427982+anhtu163@users.noreply.github.com>
2019-12-28 08:59:52 +0700

    admin tags router

---
+++ routes/admin/admin_tags_router.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var tagModel = require('../../models/tag.model')
+
+router.get('/', (req, res) => {
+    tagModel.all().then(tags => {
+        res.render('admin/vwAdmin/tags_admin', {layout: 'admin' , tags})
+    })
+
+   
+} );
+router.post('/', (req,res) =>
+{
+    var entity = {
+        tag_name: req.body.TagName,
+        tag_des: req.body.TagDes,
+        tag_slug: req.body.TagSlug,
+    }
+    tagModel.add(entity).then(rows => {
+        res.redirect('/admin/tags');
+    })
+})
+
+module.exports = router;
